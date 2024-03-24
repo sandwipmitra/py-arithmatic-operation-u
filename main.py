eb00df7fade63dabfec94d718d79abcbac16df31
SANDWIP MITRA <43679584+sandwipmitra@users.noreply.github.com>
2024-03-24 01:58:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+print(1 + 3 + 3 + 2 * 2)
+print((1 + 3 + 3 + 2) * 2)
+print(3 ** 2) #exponentiation
+print(9 % 2) #modulo-reminder
+print(7 // 2) #integer division-rounds the answer down to integer
+print(-7 // 2) #integer division-rounds the negetive answer up to integer
+
+# + Addition
+# - Subtraction
+# * Multiplication
+# / Division
+# % Mod (the remainder after dividing)
+# ** Exponentiation (note that ^ does not do this operation, as you might have seen in other languages)
+# // Divides and rounds down to the nearest integer
+
+#calculate average
+print((23 + 32 + 64) / 3)
+
+
+
+# Fill this in with an expression that calculates how many tiles are needed.
+print(((9 * 7) * 6) + ((5 * 7) * 6))
+
+# Fill this in with an expression that calculates how many tiles will be left over.
+print((588 // 17 ) - 6)
